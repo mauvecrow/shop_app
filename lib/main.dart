bd5fbf7eb9953d6e5321b401baab4ec3341c8a9d
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-12 13:07:52 -0600

    Start navigation on Product-item - go to product-details-screen - use pushNamed pattern - update main with routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/products_overview_screen.dart';
+import './screens/product_details_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +23,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+      },
     );
   }
 }
