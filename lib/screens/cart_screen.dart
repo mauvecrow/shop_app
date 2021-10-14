b5a5b58b283c8d0d1e760fdead6fdf8f6a627a4c
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-14 16:06:45 -0600

    Implement "Order NOW" handler - create Order -refactor to use Provider.of for Cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart';
+import '../providers/orders.dart';
 import '../widgets/cart_item.dart' as ci;
 
 class CartScreen extends StatelessWidget {
@@ -9,6 +10,7 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Cart cart = Provider.of<Cart>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cart'),
@@ -27,46 +29,46 @@ class CartScreen extends StatelessWidget {
                         fontSize: 20,
                       )),
                   const Spacer(),
-                  Consumer<Cart>(
-                    builder: (ctx, cart, child) => Chip(
-                      label: Text(
-                        '\$${cart.totalAmount}',
-                        style: TextStyle(
-                          color: Theme.of(context)
-                              .primaryTextTheme
-                              .headline6
-                              ?.color,
-                        ),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6?.color,
                       ),
-                      backgroundColor: Theme.of(context).colorScheme.primary,
                     ),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
                   ),
                   TextButton(
                     child: const Text("ORDER NOW"),
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Order>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
                   ),
                 ],
               ),
             ),
           ),
           const SizedBox(height: 10),
-          Consumer<Cart>(
-            builder: (ctx, cart, child) => Expanded(
-              child: ListView.builder(
-                itemBuilder: (ctx, i) {
-                  //Provider's Cart item
-                  CartItem? pci = cart.items.values.toList()[i];
-                  var productId = cart.items.keys.toList()[i];
-                  return ci.CartItem(
-                    pci.id,
-                    productId,
-                    pci.price,
-                    pci.quantity,
-                    pci.title,
-                  ); //Cart item widget
-                },
-                itemCount: cart.items.length,
-              ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (ctx, i) {
+                //Provider's Cart item
+                CartItem? pci = cart.items.values.toList()[i];
+                var productId = cart.items.keys.toList()[i];
+                return ci.CartItem(
+                  pci.id,
+                  productId,
+                  pci.price,
+                  pci.quantity,
+                  pci.title,
+                ); //Cart item widget
+              },
+              itemCount: cart.items.length,
             ),
           ),
         ],
