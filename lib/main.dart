6cd29ac797395caf2a71e36ab67a5cf04acf11e0
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-15 11:47:04 -0600

    Create User Product screen - create user product item - update main with new route - update appdrawer to include navigation

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './screens/order_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_details_screen.dart';
 import './screens/cart_screen.dart';
+import './screens/user_products_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
           CartScreen.routeName: (ctx) => const CartScreen(),
           OrderScreen.routeName: (ctx) => const OrderScreen(),
+          UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
         },
       ),
     );
