3036d1f497684b0879b5da39429fbc98f5cf77b1
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-15 11:20:26 -0600

    Add Popup when deleteing Item

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -17,6 +17,22 @@ class CartItem extends StatelessWidget {
     return Dismissible(
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog<bool>(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: const Text('Are you sure?'),
+                  content: const Text('Do you want to remove the item?'),
+                  actions: [
+                    TextButton(
+                        onPressed: () => Navigator.of(ctx).pop(false),
+                        child: const Text('No')),
+                    TextButton(
+                        onPressed: () => Navigator.of(ctx).pop(true),
+                        child: const Text('Yes')),
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
